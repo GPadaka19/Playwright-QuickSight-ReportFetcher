381c61ace4e1efb25d42d427110830b03787f238
GPadaka19 <gustipadaka19@gmail.com>
2025-02-02 00:22:18 +0700

    feat: Implement course search and date modification in AWS QuickSight dashboard

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -13,30 +13,49 @@ test('Manual CAPTCHA & MFA Test with AWS Login', async ({ page }) => {
         throw new Error('EMAIL atau PASSWORD belum ditemukan di file .env');
     }
 
-    // Navigasi ke halaman login awal
     await page.goto('https://learnerhub.skillbuilder.aws/quicksight');
 
-    // Klik tombol AWS Builder ID
     await page.getByTestId('AWSBuilderID-provider-button').click();
 
-    // Masukkan email akun
     await page.getByRole('button', { name: 'Already have AWS Builder ID?' }).click();
     await page.getByRole('textbox', { name: 'We recommend using your' }).fill(email);
     await page.getByRole('button', { name: 'Next' }).click();
 
-    // Masukkan password akun
     await page.getByRole('textbox', { name: 'Password' }).fill(password);
     await page.getByRole('button', { name: 'Sign in' }).click();
 
-    // Pause agar bisa input CAPTCHA dan MFA manual
     console.log('Silakan input CAPTCHA dan MFA secara manual');
     await page.pause();
 
-    // Pastikan halaman sudah berada di URL yang diinginkan
     await page.waitForURL('https://learnerhub.skillbuilder.aws/quicksight');
 
-    // Menunggu iframe dengan src yang dinamis
     const iframeLocator = page.locator('iframe[name*="DASHBOARD-SBARDashboardV3"]');
     await iframeLocator.waitFor({ state: 'attached' });
     await iframeLocator.waitFor({ state: 'visible' });
+
+    const frame = await iframeLocator.contentFrame();
+
+    if (frame) {
+    await frame.locator('span.ellipsis', { hasText: 'Training' }).click();
+
+    await frame.locator('role=button[name="Course All"]').click();
+
+    await frame.locator('role=combobox[name="Search value"]').fill('e-05wkr9');
+    await frame.locator('role=button[name="Search"]').click();
+
+    await frame.locator(
+    'input[aria-labelledby="E-05WKR9 - Cloud Essentials Knowledge Badge Assessment-multiSelectControl-label"]'
+    ).click();
+
+    await page.locator('body').click();
+
+    await frame.getByRole('textbox', { name: 'End Date' }).dblclick();
+    await frame.getByRole('textbox', { name: 'End Date' }).fill('');
+    await frame.getByRole('textbox', { name: 'End Date' }).fill('2025/02/02');
+    await frame.getByRole('textbox', { name: 'End Date' }).press('Enter');
+
+    await page.pause();
+    } else {
+    console.error('Iframe tidak ditemukan!');
+    }
 });
